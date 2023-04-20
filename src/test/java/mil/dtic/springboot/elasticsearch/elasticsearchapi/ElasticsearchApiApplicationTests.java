10d91b5ad0242c6e68b48fb30fce53a0b0165277
Tariq Ahsan <tariqahsan@yahoo.com>
2023-04-20 06:30:04 -0400

    first commit

---
+++ src/test/java/mil/dtic/springboot/elasticsearch/elasticsearchapi/ElasticsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mil.dtic.springboot.elasticsearch.elasticsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
